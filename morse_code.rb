22516826ea562b784bc74885a44419031240b798
ONJoseph <on.josephinfo@gmail.com>
2023-03-10 06:38:41 +0100

    Add morse code to morse_code.rb

---
+++ morse_code.rb
@@ -0,0 +1,53 @@
+require 'morse'
+
+class MorseCode
+  MORSE_CODE = {
+    ".-" => "A",
+    "-..." => "B",
+    "-.-." => "C",
+    "-.." => "D",
+    "." => "E",
+    "..-." => "F",
+    "--." => "G",
+    "...." => "H",
+    ".." => "I",
+    ".---" => "J",
+    "-.-" => "K",
+    ".-.." => "L",
+    "--" => "M",
+    "-." => "N",
+    "---" => "O",
+    ".--." => "P",
+    "--.-" => "Q",
+    ".-." => "R",
+    "..." => "S",
+    "-" => "T",
+    "..-" => "U",
+    "...-" => "V",
+    ".--" => "W",
+    "-..-" => "X",
+    "-.--" => "Y",
+    "--.." => "Z"
+  }
+
+  def self.decode_char(morse_char)
+    MORSE_CODE[morse_char]
+  end
+
+  def self.decode_word(morse_word)
+    morse_chars = morse_word.split(" ")
+    chars = morse_chars.map { |morse_char| decode_char(morse_char) }
+    chars.join("")
+  end
+
+  def self.decode(message)
+    morse_words = message.split("   ")
+    words = morse_words.map { |morse_word| decode_word(morse_word) }
+    words.join(" ")
+  end
+end
+
+# To decode the message from the old bottle, use the following IRB:
+message = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+MorseCode.decode(message)
+# Output: "ABX FULL OF S"
